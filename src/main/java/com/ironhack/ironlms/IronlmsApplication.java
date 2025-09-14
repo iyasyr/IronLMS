a41a3a8e4f9f34f38a87e69de54518de5fc42cdc
halev <halev1992@mail.ru>
2025-09-14 18:08:41 +0200

    feat: initialize project

---
+++ src/main/java/com/ironhack/ironlms/IronlmsApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.ironlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronlmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/ironlms/IronlmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.ironlms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronlmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
